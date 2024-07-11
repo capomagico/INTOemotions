803f474afecebed286da039efa4cd153fe37a8bb
Capomagico <matty.cap@live.it>
2024-07-11 17:07:36 +0200

    sito

---
+++ svelte-webcam/src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+	import { onMount } from 'svelte';
+  
+	let video;
+  
+	async function startWebcam() {
+	  video = document.querySelector('video');
+	  try {
+		const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+		video.srcObject = stream;
+	  } catch (err) {
+		console.error('Error accessing webcam:', err);
+	  }
+	}
+  
+	onMount(() => {
+	  startWebcam();
+	});
+  </script>
+  
+  <style>
+	.webcam-container {
+	  position: relative;
+	  width: 100%;
+	  padding-top: 56.25%; /* 16:9 Aspect Ratio */
+	}
+  
+	video {
+	  position: absolute;
+	  top: 0;
+	  left: 0;
+	  width: 100%;
+	  height: 100%;
+	}
+  </style>
+  
+  <div class="webcam-container">
+	<video autoplay playsinline></video>
+  </div>
+  
