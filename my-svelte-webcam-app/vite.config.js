9c881c36b48c2d2a5629df7561b0a1af237afc11
Capomagico <matty.cap@live.it>
2024-07-11 17:43:59 +0200

    Deploy to GitHub Pages

--- my-svelte-webcam-app/vite.config.js
+++ my-svelte-webcam-app/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import ghPages from "vite-plugin-gh-pages";
 
 export default defineConfig({
   base: "/INTOemotions/", // Indica il nome del repository qui
-  plugins: [svelte(), ghPages()],
+  plugins: [svelte()],
 });
